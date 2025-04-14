074314976a02306bcf2844ec03b6d5451b563a0b
anurag <anuragpathakoffl@gmail.com>
2025-04-15 05:03:00 +0530

    fix: correct import paths for action modules in index.ts

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,21 +1,21 @@
-import updateUser from "./updateUser";
+import updateUser from "./update-user";
 
-import createMessages from "./createMessages";
-import createSymptom from "./createSymptom";
-import createMedication from "./createMedication";
-import createCheckoutSession from "./createCheckoutSession";
+import createMessages from "./create-messages";
+import createSymptom from "./create-symptom";
+import createMedication from "./create-medication";
+import createCheckoutSession from "./create-checkout-session";
 
-import getHealthTips from "./getHealthTips";
-import getMessages from "./getMessages";
-import getRecommndations from "./getRecommndations";
+import getHealthTips from "./get-health-tips";
+import getMessags from "./get-messages";
+import getRecommndations from "./get-recommendations";
 
 export {
-  updateUser,
-  createMessages,
-  createSymptom,
-  createMedication,
-  getHealthTips,
-  getMessages,
-  getRecommndations,
-  createCheckoutSession,
-};
+    updateUser,
+    createMessages,
+    createSymptom,
+    createMedication,
+    getHealthTips,
+    getMessags,
+    getRecommndations,
+    createCheckoutSession,
+}
