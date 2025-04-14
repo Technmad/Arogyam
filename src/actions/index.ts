01103dc5a5669fb31b640269af39e776f102dacc
anurag <anuragpathakoffl@gmail.com>
2025-04-15 01:20:53 +0530

    feat: add index file to export action creators for user management and health recommendations

---
+++ src/actions/index.ts
@@ -0,0 +1,21 @@
+import updateUser from "./updateUser";
+
+import createMessages from "./createMessages";
+import createSymptom from "./createSymptom";
+import createMedication from "./createMedication";
+import createCheckoutSession from "./createCheckoutSession";
+
+import getHealthTips from "./getHealthTips";
+import getMessages from "./getMessages";
+import getRecommndations from "./getRecommndations";
+
+export {
+  updateUser,
+  createMessages,
+  createSymptom,
+  createMedication,
+  getHealthTips,
+  getMessages,
+  getRecommndations,
+  createCheckoutSession,
+};
